f733b4603ee616f67a103def4dd7efaec7582aa4
faqtr <d.faqtr@gmail.com>
2019-01-13 23:32:47 +0200

    проклятое представление

--- lib/app.class.php
+++ lib/app.class.php
@@ -15,10 +15,18 @@ class App {
         $controller_method = strtolower(self::$router->getMethotPrefix().self::$router->getAction());
 
         $controller_object = $controller_class;
+        
         if (method_exists($controller_object, $controller_method)){
-            $result = $controller_object->$controller_method;
+            $view_path = $controller_object->$controller_method();
+            $viev_object = new View($controller_object->getData(), $view_path);
+            $content = $viev_object->render();
         }else{
             throw new Exception('Method '.$controller_method.' of class '.$controller_object.' does not exist');
         }
+        
+        $layout = self::$router->getRoute();
+        $layout_path = VIEW_PATH.DS.$layout.'html';
+        $layout_view_object = new View(compact('content'), $layout_path);
+        echo $layout_view_object->render();
     }
 }
--- webroot/index.php
+++ webroot/index.php
@@ -2,6 +2,8 @@
 
 define ('DS', DIRECTORY_SEPARATOR);
 define ('ROOT', dirname(dirname(__FILE__)));
+define('VIEW_PATH', ROOT.DS.'views');
+
 
 require_once(ROOT.DS.'lib'.DS.'init.php');
 
